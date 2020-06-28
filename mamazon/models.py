e5f931439dce24a3d7e899202ea9e50fff57764c
omizu <tanjiryousei15812@icloud.com>
2020-06-28 19:59:26 +0900

    lesson3

---
+++ mamazon/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    price = models.IntegerField()
+    description = models.TextField()
+    thumbnail = models.ImageField(upload_to='thumbnail/')
